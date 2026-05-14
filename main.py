d3787017c63d5c483fd6b77dc7e936256da87e3a
jonathanrobin13 <jonathan.robin13@gmail.com>
2026-05-13 20:03:12 -0400

    fixed errors in main.py

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
 import openpyxl as xl
+import os
 from openpyxl.chart import BarChart, Reference
 
 
-def process_workbook(filename):
+def update_price(filename, sheet_name, min_row, max_row, percent_discount, old_column, new_column):
 
     wb = xl.load_workbook(filename)
-    sheet = wb['Sheet1']
+    sheet = wb[sheet_name]
 
-    for row in range(2, sheet.max_row + 1):
-        cell = sheet.cell(row, 3)
-        corrected_price = cell.value * 0.9
-        corrected_price_cell = sheet.cell(row, 4)
+    subtracted_price = 1.0 - percent_discount
+
+    for row in range(min_row, max_row + 1):  # 2, 5
+        cell = sheet.cell(row, old_column)
+        corrected_price = float(cell.value) * subtracted_price
+        corrected_price_cell = sheet.cell(row, new_column)
         corrected_price_cell.value = corrected_price
 
-    values = Reference(sheet,
-                       min_row=2,
-                       max_row=sheet.max_row,
-                       min_col=4,
-                       max_col=4)
-
-    chart = BarChart()
-    chart.add_data(values)
-    sheet.add_chart(chart, 'e2')
+#    values = Reference(sheet,
+#                       min_row=2,
+#                       max_row=sheet.max_row,
+#                       min_col=4,
+#                       max_col=4)
+#
+#    chart = BarChart()
+#    chart.add_data(values)
+#    sheet.add_chart(chart, 'e2')
 
     wb.save(filename)
+
+
+update_price('transactions.xlsx', 'Sheet1', 2, 4, 0.1, 2, 3)
